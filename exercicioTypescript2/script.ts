aa35fbce9a62c9a1e2bf9381df8ce12e98b8080b
matheus19012 <matheuscoreadearaujo1@gmail.com>
2023-06-21 21:57:54 -0300

    exercicio2

---
+++ exercicioTypescript2/script.ts
@@ -0,0 +1,6 @@
+function printTreeFavoriteColor():void {
+    const cor1 = prompt("insire sua primeira cor favorita");
+    const cor2 = prompt("insire sua segunda cor favorita");
+    const cor3 = prompt("insire sua terceira cor favorita");
+    console.log(cor1, cor2, cor3);
+};
