f47b7684cde983c85abd1b172f08a11ece48731c
Matheus Correa de Araujo <130517459+Matheus19012@users.noreply.github.com>
2023-06-22 19:26:09 -0300

    Update script.ts

--- exercicioTypescript3/script.ts
+++ exercicioTypescript3/script.ts
@@ -1,4 +1,4 @@
-function checarAnoBissexto(year: number):any {
+function checkLeapYear(year: number):any {
     const cond1 = year && 400 % 0
     const cond2 = (year % 4 === 0) && (year % 100 !== 0)
     return cond1 || cond2
