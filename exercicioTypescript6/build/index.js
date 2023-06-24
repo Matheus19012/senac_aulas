4f3105344cb7bceb96aa77dc630d4c1363805b6b
matheus19012 <matheuscorreadearaujo1@gmail.com>
2023-06-24 17:45:39 -0300

    exercicio6

---
+++ exercicioTypescript6/build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+function operations(num1, num2) {
+    let sum = num1 + num2;
+    let difference = num1 - num2;
+    let multiplication = num1 * num2;
+    let division = num1 / num2;
+    console.log(`Numeros recebidos: ${num1} e ${num2} 
+    \nSoma: ${sum}
+    \nSubtração: ${difference}
+    \nMultiplicação: ${multiplication} 
+    \nDivisão: ${division}`);
+}
+console.log(operations(600, 45));
+//# sourceMappingURL=index.js.map
--- exercicioTypescript6/src/index.ts
+++ exercicioTypescript6/src/index.ts
@@ -19,7 +19,7 @@
 function operations(num1: number, num2: number): void {
     let sum: number = num1 + num2;
     let difference: number = num1 - num2;
-    let multiplication: number = num1 / num2;
+    let multiplication: number = num1 * num2;
     let division: number = num1 / num2;
 
     console.log(`Numeros recebidos: ${num1} e ${num2} 
