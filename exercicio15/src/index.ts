70d8673516c1ef9154724f85c2d30e35e4c09fc8
matheus19012 <matheuscorreadearaujo1@gmail.com>
2023-06-30 21:32:22 -0300

    exercicios

---
+++ exercicio15/src/index.ts
@@ -0,0 +1,7 @@
+// const birthDate = prompt("Digite sua data de nascimento:");
+// const rgDateEmission = prompt("Digite a data de emissão de seu RG:");
+
+// function getRg(birthDate: string, rgDateEmission: string): boolean {
+
+//     if ()
+// }
